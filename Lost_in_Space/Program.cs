07c504cc068252cfe248f227893feb7a6eadaf8a
Gregg Travitz <greggtravitz@gmail.com>
2018-02-28 00:19:24 -0800

    Initial check-in of module Lost_in_Space

---
+++ Lost_in_Space/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lost_in_Space
+{
+    class Program
+    {
+        static void Main()
+        {
+            string CharacterName = "Gregg";
+
+            Console.WriteLine("Lost in Space!");
+            Console.WriteLine("Welcome to Lost in Space, an RPG where you'll be faced with decisions that affect you, your crew, and everyone you care for.");
+            Console.WriteLine("What is your name?");
+            CharacterName = Console.ReadLine();
+            Console.WriteLine("Hello, " + CharacterName + ". Good Luck.");
+            Console.ReadKey();
+
+           
+        }
+    }
+}
