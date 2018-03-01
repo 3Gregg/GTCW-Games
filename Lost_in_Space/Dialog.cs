831ebf550e8ae38fc3ce0cce8d1efb7f90d12f13
Gregg Travitz <greggtravitz@gmail.com>
2018-02-28 22:06:56 -0800

    test

---
+++ Lost_in_Space/Dialog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Lost_in_Space
+{
+    class Program
+    {
+        static void Main();
+        {
+            Dialog(CharacterName + "..." + CharacterName "! Wake up!");
+        }
+        //change the color of text when a character talks
+        static void Dialog(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write(message);
+            Console.ResetColor();        
+        }
+
+    }
+}
--- Lost_in_Space/Program.cs
+++ Lost_in_Space/Program.cs
@@ -1,21 +1,49 @@
-﻿using System;
+﻿/*
+ * Lost In Space
+ * by Gregg Travitz, February 28th, 2018
+ * 
+ * This work is a derivative of
+ * 'C# Adventure Game" by http://programmingisfun.com, used under CC BY.
+ * https://creativecommons.org/licenses/by/4.0/
+ */
+using System;
 
 namespace Lost_in_Space
 {
+    public static class Game
+    {
+        //character name
+        static string CharacterName = "Gregg";
+
+        //print out game title and overview
+        public static void StartGame()
+        {
+            Console.WriteLine("Lost in Space!");
+            Console.WriteLine("Welcome to Lost in Space, an RPG where you'll be faced with decisions that affect you, your crew, and everyone you care for.");
+            NameCharacter();
+        }
+
+        //ask player for a name, and save it
+        static void NameCharacter()
+        {
+            Console.WriteLine("What is your name?");
+
+            CharacterName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + CharacterName + ". Good Luck.");
+        }
+        
+    }
+    class Item
+    {
+        
+    }
     class Program
     {
         static void Main()
         {
-            string CharacterName = "Gregg";
-
-            Console.WriteLine("Lost in Space!");
-            Console.WriteLine("Welcome to Lost in Space, an RPG where you'll be faced with decisions that affect you, your crew, and everyone you care for.");
-            Console.WriteLine("What is your name?");
-            CharacterName = Console.ReadLine();
-            Console.WriteLine("Hello, " + CharacterName + ". Good Luck.");
+            Game.StartGame();
             Console.ReadKey();
-
-           
         }
     }
 }
